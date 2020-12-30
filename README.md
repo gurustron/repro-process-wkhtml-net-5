cee4cee7014c49da43731f154d7dcfbdcbc331b3
gurustron <gurustronpublic@gmail.com>
2020-12-30 18:27:32 +0300

    repro

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # repro-process-wkhtml-net-5
+
+Reproduces issue with starting wkhtmltopdf via ``ProcessStartInfo``:
+
+1. On linux machine (Ubuntu 20.04.1 LTS for me)
+2. Install wkhtmltopdf:
+    - sudo apt install wget xfonts-75dpi
+    - wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+    - sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
+3. Solution with NET 5.0 throws, the 3.1 one works:
+    > System.Exception: QPainter::begin(): Returned false
+   Exit with code 1, due to unknown error.
---
+++ ReproWkhml/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ReproWkhml
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "wkhtmltopdf",
+                    Arguments =
+                        "-q -B 8 -L 8 -R 8 -T 8 --print-media-type --enable-local-file-access -s Letter -O Portrait - -",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true
+                }
+            })
+            {
+                process.Start();
+
+                byte[] array;
+
+                var html = "test";
+                using (var standardInput = process.StandardInput)
+                {
+                    standardInput.WriteLine(html);
+                }
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var baseStream = process.StandardOutput.BaseStream)
+                    {
+                        var buffer = new byte[4096];
+                        int count;
+                        while ((count = baseStream.Read(buffer, 0, buffer.Length)) > 0)
+                            memoryStream.Write(buffer, 0, count);
+                    }
+
+                    var end = process.StandardError.ReadToEnd();
+                    if (memoryStream.Length == 0L)
+                        throw new Exception(end);
+                    process.WaitForExit();
+                    array = memoryStream.ToArray();
+                }
+
+                Console.WriteLine(array.Length);
+            }
+        }
+    }
+}
