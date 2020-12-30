eab920d3ec53bc3203e3c27fb0f51723a2637bff
gurustron <gurustronpublic@gmail.com>
2020-12-30 18:39:18 +0300

    fix

--- Working31/Program.cs
+++ Working31/Program.cs
@@ -2,14 +2,14 @@
 using System.Diagnostics;
 using System.IO;
 
-namespace ConsoleApp1
+namespace Working31
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            using var process = new Process
+            using (var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
@@ -22,30 +22,37 @@ namespace ConsoleApp1
                     RedirectStandardInput = true,
                     CreateNoWindow = true
                 }
-            };
-            process.Start();
-
-            byte[] array;
-            var html = "test";
-            using var standardInput = process.StandardInput;
-            standardInput.WriteLine(html);
-
-            using var memoryStream = new MemoryStream();
-            using (var baseStream = process.StandardOutput.BaseStream)
+            })
             {
-                var buffer = new byte[4096];
-                int count;
-                while ((count = baseStream.Read(buffer, 0, buffer.Length)) > 0)
-                    memoryStream.Write(buffer, 0, count);
+                process.Start();
+
+                byte[] array;
+
+                var html = "test";
+                using (var standardInput = process.StandardInput)
+                {
+                    standardInput.WriteLine(html);
+                }
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var baseStream = process.StandardOutput.BaseStream)
+                    {
+                        var buffer = new byte[4096];
+                        int count;
+                        while ((count = baseStream.Read(buffer, 0, buffer.Length)) > 0)
+                            memoryStream.Write(buffer, 0, count);
+                    }
+
+                    var end = process.StandardError.ReadToEnd();
+                    if (memoryStream.Length == 0L)
+                        throw new Exception(end);
+                    process.WaitForExit();
+                    array = memoryStream.ToArray();
+                }
+
+                Console.WriteLine(array.Length);
             }
-
-            var end = process.StandardError.ReadToEnd();
-            if (memoryStream.Length == 0L)
-                throw new Exception(end);
-            process.WaitForExit();
-            array = memoryStream.ToArray();
-
-            Console.WriteLine(array.Length);
         }
     }
 }
